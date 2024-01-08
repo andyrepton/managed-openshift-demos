3e022a91fd416206e6849d1dbfb8ff3f688fb0f8
Rhys Powell <rhys@rhyspowell.com>
2024-01-08 17:45:24 +0000

    allow edge cert to be used

--- deploying-gitops-on-rosa-or-aro/install-gitops.sh
+++ deploying-gitops-on-rosa-or-aro/install-gitops.sh
@@ -17,3 +17,7 @@ done
 
 echo "Installing GitOps"
 oc apply -f https://raw.githubusercontent.com/andyrepton/managed-openshift-demos/main/openshift-gitops/gitops-install.yaml
+
+
+# Set edge reencrypt
+oc -n openshift-gitops patch argocd/openshift-gitops --type=merge -p='{"spec":{"server":{"route":{"enabled":true,"tls":{"insecureEdgeTerminationPolicy":"Redirect","termination":"reencrypt"}}}}}'
